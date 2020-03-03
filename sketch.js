787225500a2a1b62a16c8184ead2fcccc59e8b49
gabrielchi <55122175+gabrielchi@users.noreply.github.com>
2020-03-03 16:46:48 +0800

    commit1

--- sketch.js
+++ sketch.js
@@ -4,5 +4,18 @@ function setup() {
   
   function draw() {
     background("#FFDF00");
-
+    noStroke();
+  
+  
+  //sub font
+  fill("#252525");
+  textFont("Helvetica");
+  textStyle(BOLD);
+  textSize(25);
+  text("stanley", 35, 35);
+  text("kubrick's", 40, 60);
+  
+  text("jack", 40, 590);
+  text("nicholson", 35, 615);
+  
   }
